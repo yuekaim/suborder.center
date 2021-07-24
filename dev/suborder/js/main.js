5a2249f55e5146f2d432c8a76cbec147258a9195
yuekaim <yuekaim@gmail.com>
2021-07-24 19:54:46 +0200

    scroll to about on page load

--- dev/suborder/js/main.js
+++ dev/suborder/js/main.js
@@ -58,3 +58,10 @@
 // } else {
 //     window.cancelAnimationFrame(requestId);
 // }
+
+$(document).ready(function () {
+    // Handler for .ready() called.
+    $('html, body').animate({
+        scrollTop: $('#about').offset().top
+    }, 'slow');
+});
