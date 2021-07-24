05803c71a8d13642d04ff687c63fb5e53b5bc80a
yuekaim <yuekaim@gmail.com>
2021-07-24 19:44:35 +0200

    lang test again

--- dev/suborder/home.php
+++ dev/suborder/home.php
@@ -1,17 +1,29 @@
 <@ elements/header.php @>
 
-<div id="menu">
-	<@ foreach in pagelist @>
-		<a href="/#@{id}">@{ title }</a>
-	<@ end @>
-</div>
-	<p> test </p>
+	<div id="menu">
+		<@ foreach in pagelist @>
+			<a href="/#@{id}">@{ title }</a>
+		<@ end @>
+	</div>
 
+	<@ if @{ ?lang } @>
+	    <@ set { %lang: @{ ?lang } } @>
+	<@ end @>
+
+	<@ if @{ %lang } = 'cn' and @{ +chinese } @>
 		<@ foreach in pagelist @>
 			<div class="element" id="@{id}" style="left:@{left}; top:@{top}">
 				@{ +chinese }
 			</div>
 		<@ end @>
+	<@ else @>
+		<@ foreach in pagelist @>
+			<div class="element" id="@{id}" style="left:@{left}; top:@{top}">
+				@{ +english }
+			</div>
+		<@ end @>
+	<@ end @>
+
 
 
 <@ elements/footer.php @>
